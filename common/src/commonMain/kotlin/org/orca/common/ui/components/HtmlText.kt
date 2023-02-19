a48657ebe063e5e6ea6da59168496a40e4967a4f
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-02-19 20:59:21 +1100

    remove desktop preview usage

--- common/src/commonMain/kotlin/org/orca/common/ui/components/HtmlText.kt
+++ common/src/commonMain/kotlin/org/orca/common/ui/components/HtmlText.kt
@@ -1,22 +1,13 @@
 package org.orca.common.ui.components
 
-import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.UriHandler
@@ -32,9 +23,6 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import org.jsoup.nodes.Node
 import org.jsoup.nodes.TextNode
-import org.orca.common.ui.theme.AppTheme
-import java.net.URLEncoder
-import java.nio.charset.Charset
 
 @Composable
 fun HtmlText(
