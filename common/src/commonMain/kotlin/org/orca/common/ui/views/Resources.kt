f739208bc2289000c8cf214cfcad03678d6abaf5
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-05-16 01:31:27 +1000

    remove reference to onClick modifier in resources

--- common/src/commonMain/kotlin/org/orca/common/ui/views/Resources.kt
+++ common/src/commonMain/kotlin/org/orca/common/ui/views/Resources.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.onClick
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
