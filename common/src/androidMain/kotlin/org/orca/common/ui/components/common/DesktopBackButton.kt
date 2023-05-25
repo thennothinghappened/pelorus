3e482b64fdd46b36fd9800b3e801011bd873a6de
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-05-25 22:01:52 +1000

    Remove deprecated DesktopBackButton

--- common/src/androidMain/kotlin/org/orca/common/ui/components/common/DesktopBackButton.kt
+++
@@ -1,6 +0,0 @@
-package org.orca.common.ui.components.common
-
-import androidx.compose.runtime.Composable
-
-@Composable
-actual fun DesktopBackButton(onBackPress: () -> Unit) {}
--- common/src/commonMain/kotlin/org/orca/common/ui/components/common/DesktopBackButton.kt
+++
@@ -1,7 +0,0 @@
-package org.orca.common.ui.components.common
-
-import androidx.compose.runtime.Composable
-
-@Deprecated("Replace with a proper Scaffold")
-@Composable
-expect fun DesktopBackButton(onBackPress: () -> Unit)
