8d6d754550200f7a5ef7c86e4f6039a5a7fa9bb8
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-11-24 09:33:24 +1100

    add `kotlass`, fixed local.properties github packages key, testing K2!

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
+import java.io.FileInputStream
+import java.io.InputStreamReader
+import java.util.Properties
+import kotlin.io.path.toPath
+
 rootProject.name = "pelorus"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
@@ -10,12 +15,39 @@ pluginManagement {
     }
 }
 
+val properties = File(rootDir
+    .resolve("local.properties")
+    .toURI()).let {
+    if (it.isFile) {
+        val properties = Properties()
+
+        InputStreamReader(FileInputStream(it), Charsets.UTF_8).use { reader ->
+            properties.load(reader)
+        }
+
+        properties
+    } else null
+}
+
+val gitHubUser: String? = properties?.getProperty("GITHUB_USER") ?: System.getenv("GITHUB_USER")
+val gitHubToken: String? = properties?.getProperty("GITHUB_TOKEN") ?: System.getenv("GITHUB_TOKEN")
+
 dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://jogamp.org/deployment/maven")
+
+        if (gitHubUser != null && gitHubToken != null) {
+            maven {
+                url = uri("https://maven.pkg.github.com/thennothinghappened/kotlass")
+                credentials {
+                    username = gitHubUser
+                    password = gitHubToken
+                }
+            }
+        }
     }
 }
 
