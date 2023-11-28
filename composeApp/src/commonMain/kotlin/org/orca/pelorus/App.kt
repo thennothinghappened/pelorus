d0c7e467b7695d61e4ca16950e55653fffc3503b
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2023-11-28 17:28:26 +1100

    Add proper logo

--- composeApp/src/commonMain/kotlin/org/orca/pelorus/App.kt
+++ composeApp/src/commonMain/kotlin/org/orca/pelorus/App.kt
@@ -14,7 +14,6 @@ import org.orca.pelorus.ui.PelorusAppTheme
 
 @Composable
 fun App() {
-
     ProvideStrings {
         PelorusAppTheme(darkTheme = isSystemInDarkTheme()) {
             Surface(Modifier.fillMaxSize()) {
