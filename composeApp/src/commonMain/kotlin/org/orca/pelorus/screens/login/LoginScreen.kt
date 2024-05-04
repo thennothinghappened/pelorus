c2186ac25c52e71deb57fa46729dd710900bbbdb
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-05-04 13:40:55 +1000

    feat: Login text

--- composeApp/src/commonMain/kotlin/org/orca/pelorus/screens/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/orca/pelorus/screens/login/LoginScreen.kt
@@ -3,26 +3,53 @@ package org.orca.pelorus.screens.login
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.stringResource
 import org.orca.pelorus.screens.login.manual.ManualLoginScreen
+import org.orca.pelorus.ui.common.MediumHorizontalDivider
+import pelorus.composeapp.generated.resources.Res
+import pelorus.composeapp.generated.resources.login_tagline
+import pelorus.composeapp.generated.resources.login_welcome
 
 /**
  * Initial login screen for the application, basically handles top-level authentication and loading from there.
  */
 object LoginScreen : Screen {
 
+    @OptIn(ExperimentalResourceApi::class)
     @Composable
     override fun Content() {
 
         val navigator = LocalNavigator.currentOrThrow
 
         Column(Modifier.fillMaxSize()) {
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(Res.string.login_welcome),
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Text(
+                    text = stringResource(Res.string.login_tagline),
+                    style = MaterialTheme.typography.titleSmall
+                )
+            }
+
+            MediumHorizontalDivider()
 
             ListItem(
                 headlineContent = {
