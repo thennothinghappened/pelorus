6e581e236aae394236de9c5889d379162541560b
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-05-06 19:46:33 +1000

    docs: Add javadoc blurb for IAuthedServices

--- composeApp/src/commonMain/kotlin/org/orca/pelorus/data/services/authed/IAuthedServices.kt
+++ composeApp/src/commonMain/kotlin/org/orca/pelorus/data/services/authed/IAuthedServices.kt
@@ -4,7 +4,11 @@ import androidx.compose.runtime.Composable
 import org.orca.pelorus.screens.home.HomeScreenModel
 
 /**
- * The main app authenticated-scope services.
+ * The main app authenticated-scope services provider.
+ *
+ * This mostly provides screen models as the UI-facing side of operations, but those
+ * are given the dependencies they need implicitly by shared repositories without the UI needing
+ * to be involved in this.
  */
 interface IAuthedServices {
 
